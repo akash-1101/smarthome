8d254e2e6cd06cfaaa7054d2fcc74abea74b2bc3
Akash <aakash@siqsess.com>
2022-03-22 11:30:09 +0530

    card fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -39,73 +40,361 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          padding: EdgeInsets.all(30),
           decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/background.jpg"),
-                  fit: BoxFit.fill)),
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.purple,
+              Colors.blueAccent,
+            ],
+          )),
           child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.only(top: 10),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+                      Container(
+                          color: Colors.transparent,
+                          margin: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Good Morning",
+                                style: TextStyle(
+                                    fontSize: 28,
+                                    color: Colors.white.withOpacity(0.8)),
+                              ),
+                              Text(
+                                "Akash A",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    color: Theme.of(context).cardColor),
+                              ),
+                            ],
+                          )),
                       Card(
-                        elevation: 10,
-                        color: HexColor("#f0a76a"),
-                        shadowColor: Colors.transparent,
-                        child: Container(
-                            margin: EdgeInsets.all(20),
-                            padding: EdgeInsets.only(left: 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Good Morning",
-                                  style: TextStyle(
-                                      fontSize: 28,
-                                      color: Theme.of(context).backgroundColor),
-                                ),
-                                Text(
-                                  "Akash A",
-                                  style: TextStyle(
-                                      fontSize: 32,
-                                      color: Theme.of(context).cardColor),
-                                ),
-                              ],
-                            )),
-                      ),
-                      Card(
-                        elevation: 20,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
+                        color: Colors.white10,
+                        shadowColor: Colors.white10,
+                        elevation: 50,
                         child: Container(
                           decoration: BoxDecoration(
                               shape: BoxShape.rectangle,
-                              color: HexColor("#f0a76a"),
+                              color: Colors.transparent,
                               borderRadius: BorderRadius.circular(10),
                               border: Border.all(color: Colors.white)),
                           child: IconButton(
                             onPressed: () {},
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.add,
                               color: Colors.white,
                             ),
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    "Living Room",
+                    style: TextStyle(
+                        fontSize: 20, color: Theme.of(context).cardColor),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Card(
+                        color: Colors.white,
+                        elevation: 50,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Container(
+                          height: 220,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.white)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Home Temperature",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                "23 \u2103",
+                                style: TextStyle(
+                                    fontSize: 40, fontWeight: FontWeight.bold),
+                              ),
+                              CupertinoSwitch(
+                                value: true,
+                                activeColor: Colors.purple,
+                                onChanged: (value) {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Card(
+                        color: Colors.white24,
+                        shadowColor: Colors.white24,
+                        elevation: 50,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Container(
+                          height: 220,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.white)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Plug Wall",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ListTileWidget("Macbook"),
+                                    ListTileWidget("HomePod"),
+                                    ListTileWidget("Playstation"),
+                                  ],
+                                ),
+                              ),
+                              CupertinoSwitch(
+                                value: false,
+                                thumbColor: Colors.white,
+                                activeColor: Colors.purple,
+                                onChanged: (value) {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Card(
+                        color: Colors.white24,
+                        shadowColor: Colors.white24,
+                        elevation: 50,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Container(
+                          height: 150,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.white)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Plug Wall",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ListTileWidget("Macbook"),
+                                  ],
+                                ),
+                              ),
+                              CupertinoSwitch(
+                                value: false,
+                                thumbColor: Colors.white,
+                                activeColor: Colors.purple,
+                                onChanged: (value) {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Card(
+                        color: Colors.white24,
+                        shadowColor: Colors.white24,
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Container(
+                          height: 150,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.white)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Smart Tv",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white),
+                                      ),
+                                      Text(
+                                        "Sony Tv",
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [],
+                                ),
+                              ),
+                              CupertinoSwitch(
+                                value: false,
+                                thumbColor: Colors.white,
+                                activeColor: Colors.purple,
+                                onChanged: (value) {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ListTileWidget extends StatelessWidget {
+  final String title;
+  const ListTileWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      child: ListTile(
+          leading: const MyBullet(),
+          minVerticalPadding: 0,
+          contentPadding: const EdgeInsets.all(2),
+          title: Transform.translate(
+            offset: const Offset(-30, -4),
+            child: Center(
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class MyBullet extends StatelessWidget {
+  const MyBullet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 12.0,
+      width: 12.0,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
       ),
     );
   }
